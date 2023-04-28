a8a305d60e8a1350420e568fe324215bf146bd87
asaveiflorin <asaveiflorin@gmail.com>
2023-04-28 18:38:23 +0300

    dummy about page

--- static-react-homepage/src/pages/about/AboutPage.tsx
+++ static-react-homepage/src/pages/about/AboutPage.tsx
@@ -1,5 +1,31 @@
 export const AboutPage = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="bg-gray-100 min-h-screen">
+        <div className="container mx-auto py-16 px-4">
+          <h1 className="text-3xl font-bold mb-4">About Us</h1>
+          <p className="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies sapien ac
+            tortor tincidunt, id pharetra arcu malesuada. Praesent eleifend lectus vel nisi
+            finibus, sed dignissim neque pellentesque. Vivamus bibendum nunc vitae velit
+            imperdiet, eu rhoncus est pulvinar. Sed ultrices tortor tellus, eu dignissim
+            nisi interdum eget.
+          </p>
+          <p className="mb-4">
+            Nulla facilisi. Proin non turpis ac lacus dictum faucibus eu at enim. Sed
+            venenatis massa et justo ultricies euismod. Nulla facilisi. Sed fringilla
+            interdum enim, sit amet pharetra nibh. Etiam accumsan purus vitae justo
+            bibendum, ac placerat risus blandit. Integer tristique dolor id velit sagittis
+            viverra.
+          </p>
+          <p className="mb-4">
+            Quisque in nulla vel felis finibus malesuada. Suspendisse eget sagittis quam.
+            Donec ac eros a velit hendrerit tristique in vel justo. Sed tincidunt turpis
+            magna, eu lobortis enim hendrerit vel. Morbi sit amet diam purus. Donec
+            bibendum turpis libero, a porttitor lectus pharetra id.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
